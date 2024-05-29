5b50e4ef87514a16bf00466ec4db807cb53e8fce
SejunKang <sejungang2@gmail.com>
2024-05-29 21:58:59 +0900

    Create Tracking.cc

---
+++ src/Tracking.cc
@@ -0,0 +1,29 @@
+
+#include <iostream>
+#include "Tracking.hh"
+#include "G4Track.hh"
+#include "G4VProcess.hh"
+
+/*
+Tracking::Tracking()
+{
+}
+
+Tracking::~Tracking()
+{
+}
+
+void Tracking::UserSteppingAction(const G4Step* step)
+{
+	if ( step -> GetTrack() -> GetTrackID() != 1 )
+	{
+	std::cout
+		<< "Particle: "
+		<< step -> GetTrack() -> GetParticleDefinition() -> GetParticleName()
+		<< "  From: "
+
+		<<step -> GetTrack() -> GetCreatorProcess() -> GetProcessName() 
+		<< std::endl;
+	}
+}
+*/
