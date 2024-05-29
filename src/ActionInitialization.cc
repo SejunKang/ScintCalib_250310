fffdcd4951e512d3a04999676e5d8660c7b8e5ef
SejunKang <sejungang2@gmail.com>
2024-05-29 21:33:07 +0900

    Create ActionInitialization.cc

---
+++ src/ActionInitialization.cc
@@ -0,0 +1,31 @@
+#include "ActionInitialization.hh"
+#include "PrimaryGeneratorAction.hh"
+#include "RunAction.hh"
+#include "EventAction.hh"
+#include "SteppingAction.hh"
+
+
+ActionInitialization::ActionInitialization()
+    : G4VUserActionInitialization()
+{
+}
+
+ActionInitialization::~ActionInitialization()
+{
+}
+
+void ActionInitialization::BuildForMaster() const
+{
+    SetUserAction(new RunAction);
+}
+
+void ActionInitialization::Build() const
+{
+    SetUserAction(new PrimaryGeneratorAction);
+    SetUserAction(new RunAction());
+    EventAction* EA = new EventAction();
+    SetUserAction(EA);
+    SetUserAction(new SteppingAction(EA));
+    //SetUserAction(new Tracking);
+
+}
