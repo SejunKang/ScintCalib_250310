e8278f8f7eb0a44dab7c4740a2c46bd3d0570c8b
SejunKang <sejungang2@gmail.com>
2024-05-29 22:03:06 +0900

    Create SteppingAction.hh

---
+++ include/SteppingAction.hh
@@ -0,0 +1,27 @@
+/*
+#ifndef STEPPINGACTION_h
+#define STEPPINGACTION_h 1
+*/
+
+#ifndef STEACT_h
+#define STEACT_h 1
+
+#include "G4UserSteppingAction.hh"
+
+#include "EventAction.hh"
+
+class EventAction;
+
+class SteppingAction: public G4UserSteppingAction
+{
+  public:
+	SteppingAction(EventAction* EA);
+	virtual ~SteppingAction();
+
+	virtual void UserSteppingAction(const G4Step*);
+
+  private:
+	EventAction* m_EA;
+};
+
+#endif
