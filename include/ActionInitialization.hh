b66a1edb5af4727154b8300c864a947af0be38e5
SejunKang <sejungang2@gmail.com>
2024-05-29 22:00:32 +0900

    Create ActionInitialization.hh

---
+++ include/ActionInitialization.hh
@@ -0,0 +1,22 @@
+/*
+#ifndef ACTIONINITIALIZATION_HH
+#define ACTIONINITIALIZATION_HH
+*/
+#ifndef ACTINI_h
+#define ACTINI_h 1
+
+#include "G4VUserActionInitialization.hh"
+#include "RunAction.hh"
+#include "G4String.hh"
+
+class ActionInitialization : public G4VUserActionInitialization
+{
+public:
+    ActionInitialization();
+    virtual ~ActionInitialization() override;
+
+    virtual void BuildForMaster() const override;
+    virtual void Build() const override;
+};
+
+#endif
